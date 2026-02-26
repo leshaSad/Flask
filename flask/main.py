d362c338cf1425d1f7e853547d0db1273cac5e8d
sadal <leshasadretdinov@gmail.com>
2026-02-26 16:39:40 +0300

    update

---
+++ flask/main.py
@@ -0,0 +1,138 @@
+import json
+
+from flask import Flask, url_for, render_template, redirect
+
+from forms.loginform import LoginForm
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+@app.route('/')
+@app.route('/index')
+def index(username):
+    param = {}
+    param['text1'] = "Миссия Колонизация Марса"
+    param['text2'] = "И на Марсе будут яблони цвести!"
+    param['title'] = username
+    return render_template('index.html', **param)
+
+@app.route('/odd_even')
+def odd_even():
+    return render_template('odd_even.html', number=10)
+
+@app.route('/success')
+def success():
+    return 'OK'
+
+@app.route('/news')
+def news():
+    with open("news.json", "rt", encoding="utf8") as f:
+        news_list = json.loads(f.read())
+    print(news_list)
+    return render_template('news.html', news=news_list)
+
+@app.route('/promotion')
+def promotion():
+    rec = ['Человечество вырастает из детства.', 'Человечеству мала одна планета.',
+           'Мы сделаем обитаемыми безжизненные пока планеты.', 'И начнем с Марса!',
+           'Присоединяйся!']
+    return '</br>'.join(rec)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('login.html', title='Авторизация', form=form)
+
+@app.route('/image_sample')
+def image():
+    return f'''<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                    <title>Привет, Марс!</title>
+                  </head>
+                  <body>
+                    <h1>Жди нас, Марс!</h1>
+                    <img src="{url_for('static', filename='img/mars.jpg')}"
+                        class="img-fluid"
+                        alt="здесь должна была быть картинка, но не нашлась"
+                        width=400>
+                    <div>Вот она какая, красная планета.</dif>
+                  </body>
+                </html>
+    
+    
+           '''
+
+
+@app.route('/sample_page')
+def return_sample_page():
+    return f"""<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                    <title>Привет, Яндекс!</title>
+                  </head>
+                  <body>
+                    <h1>Первая HTML-страница</h1>
+                  </body>
+                </html>"""
+
+
+@app.route('/bootstrap_sample')
+def bootstrap():
+    return '''<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                    <link rel="stylesheet"
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                    crossorigin="anonymous">
+                    <title>Привет, Яндекс!</title>
+                  </head>
+                  <body>
+                    <h1>Привет, Яндекс!</h1>
+                    <div class="alert alert-primary" role="alert">
+                      А мы тут компонентами Bootstrap балуемся
+                    </div>
+                  </body>
+                </html>'''
+
+
+i = 0
+
+
+@app.route('/i')
+def show_i():
+    global i
+    i += 1
+    return str(i)
+
+
+@app.route('/greeting/<username>')
+def greeting(username):
+    return f'''<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                   <link rel="stylesheet"
+                   href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                   integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                   crossorigin="anonymous">
+                    <title>Привет, {username}</title>
+                  </head>
+                  <body>
+                    <h1>Привет, {username}!</h1>
+                  </body>
+                </html>'''
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
